08030404d5b7b6d0a4eb4b3d3b7adbce2f415b9c
Bin Zhu <75013190+BinZhu-ece@users.noreply.github.com>
2023-11-28 21:43:49 +0800

    Update Step3_compute_scores_T5.py

--- Step3_compute_scores_T5.py
+++ Step3_compute_scores_T5.py
@@ -3,7 +3,7 @@ import pprint
 import traceback
 import tqdm
 import argparse
-
+import csv
 def T5_json(args):
     folder_list = [ os.path.join(args.T5_judge_files_folder, sub_folder) for sub_folder in os.listdir(args.T5_judge_files_folder)]
     dataset_score_dict = {}
@@ -31,27 +31,27 @@ def compute_scores(args, dataset_score_dict):
   dataset_weight = {
     1:
       {
-      # "ActivityNet":1,
-      # "MSVD":1,
-      # "MSRVTT":1,
-      # "TGIF":1,
-      # "Youcook2":1,
-      # "Ucfcrime":1,
+      "ActivityNet":1,
+      "MSVD":1,
+      "MSRVTT":1,
+      "TGIF":1,
+      "Youcook2":1,
+      "Ucfcrime":1,
       "MOT":0.5,
       },
     
     2:
       {
-        # "TVQA":1,
-        # "MV":1,
-        # "NBA":1,
+        "TVQA":1,
+        "MV":1,
+        "NBA":1,
       },
       
     3:
       {
-        # "Driving-exam":0.5,
-        # "Driving-decision-making":1,
-        # "SQA3D":1,
+        "Driving-exam":0.5,
+        "Driving-decision-making":1,
+        "SQA3D":1,
       }
     
   }
@@ -61,7 +61,6 @@ def compute_scores(args, dataset_score_dict):
   exclusive_understanding_weight = dataset_weight[1]
   weights_sum = sum(exclusive_understanding_weight.values())
   exclusive_understanding_score = 0
-  # import ipdb; ipdb.set_trace()
   for dataset_name, weight in exclusive_understanding_weight.items():
     exclusive_understanding_score += weight * dataset_score_dict[dataset_name] / weights_sum
   
@@ -88,17 +87,41 @@ def compute_scores(args, dataset_score_dict):
   final_score = sum([exclusive_understanding_score, prior_QA_score, com_and_dec_QA_score])/3
   dataset_score_dict['final_score'] = final_score
     
+  # ========================
+  data = [
+    
+    ["Avg. All", "Avg. Video-Exclusive", "Avg. Prior-Knowledge QA", "Avg. Decision-Making", 
+     "ActivityNet", "MSVD", "MSRVTT", "TGIF", "Youcook2", "Ucfcrime", 
+     "MOT", "TVQA", "MV", "NBA", "Driving-exam", "Driving-decision-making", "SQA3D"],
+    
+    [ final_score, exclusive_understanding_score, prior_QA_score, com_and_dec_QA_score, 
+      dataset_score_dict['ActivityNet'], 
+      dataset_score_dict["MSVD"],
+      dataset_score_dict['MSRVTT'],
+      dataset_score_dict['TGIF'],
+      dataset_score_dict['Youcook2'],
+      dataset_score_dict['Ucfcrime'],
+      dataset_score_dict['MOT'],
+      dataset_score_dict['TVQA'],
+      dataset_score_dict['MV'],
+      dataset_score_dict['NBA'],
+      dataset_score_dict['Driving-exam'],
+      dataset_score_dict['Driving-decision-making'],
+      dataset_score_dict['SQA3D'],
+     ],
+  ]
   
-  print(dataset_score_dict)
-  with open(args.score_output_file, 'w', encoding='utf-8') as f:
-    json.dump(dataset_score_dict, f, indent=2)
-  print(f'{args.score_output_file} is saved!')
+  with open(args.score_output_file, "w", newline="") as csvfile:
+    writer = csv.writer(csvfile)
+    writer.writerows(data)
+
+  print(f"{args.score_output_file} saved successfully.")
     
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--T5_judge_files_folder", type=str, default="/remote-home/share/VideoBenchmark/Video_Benchmark/VLLM-3metrics/Video-LLaVA/T5_Judge")
-  parser.add_argument("--score_output_file", type=str, default="./Final_score_table_T5.json")
+  parser.add_argument("--score_output_file", type=str, default="./Final_score_table_T5.csv")
   args = parser.parse_args()
   dataset_score_dict = T5_json(args)
   compute_scores(args, dataset_score_dict)
